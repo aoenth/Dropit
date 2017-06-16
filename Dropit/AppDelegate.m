94c36e4c87be36ce3f6a2ffd4d966ec948b0944b
Kevin Peng <aoenth@gmail.com>
2017-06-15 21:16:06 -0400

    Updates with Fingertips

--- Dropit/AppDelegate.m
+++ Dropit/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Fingertips/MBFingerTipWindow.h>
 
 @interface AppDelegate ()
 
@@ -14,12 +15,15 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    UIViewController *vc = self.window.rootViewController;
+    CGRect frame = [[UIScreen mainScreen] bounds];
+    self.window = [[MBFingerTipWindow alloc] initWithFrame:frame];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
     return YES;
 }
-
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
